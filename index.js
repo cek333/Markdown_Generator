de7ded56b95252a79319a12b7ed6498c4b6bb86c
cek333 <69444825+cek333@users.noreply.github.com>
2020-10-25 22:21:48 -0400

    added inquirer and questions

--- index.js
+++ index.js
@@ -1,6 +1,60 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 // array of questions for user
 const questions = [
-
+  {
+    name: 'github',
+    message: 'GitHub Name:'
+  },
+  {
+    name: 'email',
+    message: 'Email Address:'
+  },
+  {
+    name: 'title',
+    message: 'Project Title:'
+  },
+  {
+    name: 'description',
+    message: 'Project Description:'
+  },
+  {
+    name: 'install',
+    message: 'Installation Procedure:'
+  },
+  {
+    type: 'editor',
+    name: 'usage',
+    message: 'Usage Instructions:'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license:',
+    choices: [
+      {
+        name: 'Apache License 2.0',
+        value: 'apache'
+      },
+      {
+        name: 'GNU General Public License v3.0',
+        value: 'gpl3'
+      },
+      {
+        name: 'MIT License',
+        value: 'mit'
+      }
+    ]
+  },
+  {
+    name: 'contributing',
+    message: 'Contributing Instructions:'
+  },
+  {
+    name: 'tests',
+    message: 'Testing Instructions:'
+  }
 ];
 
 // function to write README file
@@ -8,9 +62,10 @@ function writeToFile(fileName, data) {
 }
 
 // function to initialize program
-function init() {
-
+async function main() {
+  let data = await inquirer.prompt(questions);
+  console.log(data);
 }
 
 // function call to initialize program
-init();
+main();
